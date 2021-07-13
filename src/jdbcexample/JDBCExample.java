7528ed11793a1fd5f38b3461495c6c857978b57e
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-07-13 17:25:23 +0200

    Fatte varie query

--- src/jdbcexample/JDBCExample.java
+++ src/jdbcexample/JDBCExample.java
@@ -4,25 +4,40 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCExample {
 
     public static void main(String[] args) {
         Connection con = null;
         Statement stmt = null;
+        List<String> lista =  new ArrayList<>();
         try {
             con = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/jdbcrand",
                     "root",
                     "");
+            Statement canc = con.createStatement();
+            String delSql = "delete from Persons";
+            int cancellati = canc.executeUpdate(delSql);
+            System.out.println("Rimossi " + cancellati);
+            Statement ins = con.createStatement();
+            String insSql = "insert into Persons values (1, \"Giuseppe\",\"Garibaldi\",\"Via Quarto, 1000\",\"Palermo\" )";
+            boolean bins = ins.execute(insSql);
+            Statement ins1 = con.createStatement();
+            String insSql1 = "insert into Persons values (1, \"Camillo\",\"Cavour\",\"Via Torino, 1\",\"Torino\" )";
+            int numIns = ins.executeUpdate(insSql1);
+            System.out.println("Puff! E` andata!" + bins+" aggiunti "+ numIns);
             stmt = con.createStatement();
-            String sql = "select * from cassa";
+            String sql = "select * from persons";
             ResultSet res = stmt.executeQuery(sql);
             while (res.next()) {
-                long id = res.getLong("id");
-                String codice = res.getString("codice");
-                long fk = res.getLong("scontrino_id");
-                System.out.println("id = "+ id + "; codice = "+ codice + "; fk = " + fk);
+                System.out.println("Siamo qui");
+                long id = res.getLong("PersonId");
+                String fn = res.getString("FirstName");
+                String ln = res.getString("LastName");
+                System.out.println("id = "+ id + "; Nome = "+ fn + "; Cognome = " + ln);
             }
         } catch (Exception e) {
             e.printStackTrace();
